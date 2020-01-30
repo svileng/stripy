8c390b9dcc3fb823770b43c872a7120070981f92
Tom Conroy <tom@conroy.com.au>
2020-01-30 15:30:57 -0600

    Allow headers to be set via opts arg

--- lib/stripy.ex
+++ lib/stripy.ex
@@ -24,11 +24,38 @@ defmodule Stripy do
   check out "stripity_stripe" or "stripe_elixir" on Hex.
   """
 
-  @doc "Constructs HTTPoison header list with auth."
-  def headers(%{secret_key: sk, version: v}) do
-    [{"Authorization", "Bearer #{sk}"},
-     {"Content-Type", "application/x-www-form-urlencoded"},
-     {"Stripe-Version", v}]
+  @doc """
+  Constructs HTTPoison header list.
+
+  ## Options
+
+   * `:secret_key` - Sets the `Authorization` header with a stripe Secret Key
+    for this request
+
+   * `:version` - override the `Stripe-Version` header, defaults to `2017-06-05`
+
+   * `:stripe_account` - sets the `Stripe-Account` header for use with
+    [Stripe Connect](https://stripe.com/docs/connect/authentication#stripe-account-header)
+
+   * `:idempotency_key` - sets the
+    [`Idempotency-Key` header](https://stripe.com/docs/api/idempotent_requests)
+  """
+  def headers(params) do
+    base_headers = [{"Content-Type", "application/x-www-form-urlencoded"}]
+
+    Enum.reduce(params, base_headers, fn
+      {:secret_key, sk}, headers ->
+        [{"Authorization", "Bearer #{sk}"} | headers]
+
+      {:version, v}, headers ->
+        [{"Stripe-Version", v} | headers]
+
+      {:stripe_account, id}, headers ->
+        [{"Stripe-Account", id} | headers]
+
+      {:idempotency_key, key}, headers ->
+        [{"Idempotency-Key", key} | headers]
+    end)
   end
 
   @doc "Constructs url with query params from given data."
@@ -42,6 +69,8 @@ defmodule Stripy do
   Will return an HTTPoison standard response; see `parse/1`
   for decoding the response body.
 
+  See `headers/1` for a list of options.
+
   ## Examples
       iex> Stripy.req(:get, "subscriptions")
       {:ok, %HTTPoison.Response{...}}
@@ -49,15 +78,18 @@ defmodule Stripy do
       iex> Stripy.req(:post, "customers", %{"email" => "a@b.c", "metadata[user_id]" => 1})
       {:ok, %HTTPoison.Response{...}}
   """
-  def req(action, resource, data \\ %{}) when action in [:get, :post, :delete] do
+  def req(action, resource, data \\ %{}, opts \\ []) when action in [:get, :post, :delete] do
     if Application.get_env(:stripy, :testing, false) do
       mock_server = Application.get_env(:stripy, :mock_server, Stripy.MockServer)
       mock_server.request(action, resource, data)
     else
-      header_params = %{
-        secret_key: Application.fetch_env!(:stripy, :secret_key),
-        version: Application.get_env(:stripy, :version, "2017-06-05")
-      }
+      header_params =
+        [
+          secret_key: Application.fetch_env!(:stripy, :secret_key),
+          version: Application.get_env(:stripy, :version, "2017-06-05")
+        ]
+        |> Keyword.merge(opts)
+
       api_url = Application.get_env(:stripy, :endpoint, "https://api.stripe.com/v1/")
       options = Application.get_env(:stripy, :httpoison, [])
 
@@ -70,9 +102,11 @@ defmodule Stripy do
   def parse({:ok, %{status_code: 200, body: body}}) do
     {:ok, Poison.decode!(body)}
   end
+
   def parse({:ok, %{body: body}}) do
     error = Poison.decode!(body) |> Map.fetch!("error")
     {:error, error}
   end
+
   def parse({:error, error}), do: {:error, error}
 end
